5629cf9e4213e0949a98ca4d436df0c211a963f9
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-05-14 13:10:50 -0700

    Milestone 3

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <list>
 using namespace std;
 
 // Function prototype
-int sum_ascii(string s);
+int gen_hash_index(string code);
 
 int main() {
 
@@ -15,32 +17,60 @@ int main() {
     string code;
     long long grand_total = 0;
 
+    // Hash table: key = int, value = list of strings
+    map<int, list<string>> hash_table;
+
     if (!fin) {
         cout << "Error opening file." << endl;
         return 1;
     }
 
-    // Read each 12-character code
+    // Read each code
     while (fin >> code) {
-        int value = sum_ascii(code);
-        grand_total += value;
+
+        int hash_index = gen_hash_index(code);
+
+        grand_total += hash_index;
+
+        // Insert into hash table
+        hash_table[hash_index].push_back(code);
     }
 
     fin.close();
 
     // Output grand total
-    cout << "Grand total: " << grand_total << endl;
+    cout << "Grand total: " << grand_total << endl << endl;
+
+    // Display first 100 map entries
+    cout << "First 100 hash table entries:" << endl;
+
+    int counter = 0;
+
+    for (auto pair : hash_table) {
+
+        cout << "Hash Index: " << pair.first << " -> ";
+
+        for (string s : pair.second) {
+            cout << s << " ";
+        }
+
+        cout << endl;
+
+        counter++;
+        if (counter >= 100)
+            break;
+    }
 
     return 0;
 }
 
 // Function definition
-int sum_ascii(string s) {
+int gen_hash_index(string code) {
     int sum = 0;
 
-    for (char ch : s) {
+    for (char ch : code) {
         sum += (int) ch;
     }
 
     return sum;
-}
+} 
