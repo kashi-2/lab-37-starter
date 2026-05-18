108bfd988d548a947d76550d7bf651f81dbdd2ec
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-05-17 17:48:18 -0700

    Milestone 1

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 // Function prototype
 int gen_hash_index(string code);
+void print_first_100(map<int, list<string>>& hash_table);
 
 int main() {
 
@@ -44,25 +45,43 @@ int main() {
     // Display first 100 map entries
     cout << "First 100 hash table entries:" << endl;
 
-    int counter = 0;
+ int choice;
 
-    for (auto pair : hash_table) {
-
-        cout << "Hash Index: " << pair.first << " -> ";
-
-        for (string s : pair.second) {
-            cout << s << " ";
-        }
+    do {
 
         cout << endl;
+        cout << "========== HASH TABLE MENU ==========" << endl;
+        cout << "1. Print first 100 entries" << endl;
+        cout << "2. Search for a key" << endl;
+        cout << "3. Add a key" << endl;
+        cout << "4. Remove a key" << endl;
+        cout << "5. Modify a key" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
 
-        counter++;
-        if (counter >= 100)
-            break;
-    }
+        while (choice < 1 || choice > 6) {
+            cout << "Invalid choice. Enter 1-6: ";
+            cin >> choice;
+        }
+
+        switch (choice) {
+
+            case 1:
+                print_first_100(hash_table);
+                break;
+
+            case 6:
+                cout << "Exiting program." << endl;
+                break;
+        }
+
+    } while (choice != 6);
 
     return 0;
 }
+
+
 
 // Function definition
 int gen_hash_index(string code) {
@@ -74,3 +93,30 @@ int gen_hash_index(string code) {
 
     return sum;
 } 
+
+void print_first_100(map<int, list<string>>& hash_table) {
+
+    cout << endl;
+    cout << "First 100 hash table entries:" << endl;
+
+    int counter = 0;
+
+    for (auto pair : hash_table) {
+
+        cout << "Hash Index: "
+             << pair.first
+             << " -> ";
+
+        for (string s : pair.second) {
+            cout << s << " ";
+        }
+
+        cout << endl;
+
+        counter++;
+
+        if (counter >= 100) {
+            break;
+        }
+    }
+}
